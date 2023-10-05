7797448fa1f2cdf788ce9354477f8e25f964d9d6
tony1611 <antbarrantes@estudiantec.cr>
2023-10-04 22:39:26 -0600

    Initial file structure

---
+++ src/main/java/com/mycompany/thewalkingtec/TheWalkingTEC.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.thewalkingtec;
+
+/**
+ *
+ * @author barra
+ * @author ayme
+ */
+public class TheWalkingTEC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
